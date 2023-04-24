bb7a592a2b4714653d402a131ec517431751846f
SatyakiPakrashi <satyakipakrashi8102@gmail.com>
2023-04-24 18:05:04 +0530

    newsapp

---
+++ screens/Home.tsx
@@ -0,0 +1,105 @@
+//@ts-nocheck
+
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Appbar,Chip,Button } from 'react-native-paper'
+import {useTheme} from 'react-native-paper'
+
+const categories =["Technology","Sports","Politics","Health","Buisness"]
+
+const API_KEY ="pub_20823a015d8d3353c886a3b647b1f487ee004";
+const Home = () => {
+const [newsData, setNewsData] = useState({})
+  
+  const theme= useTheme();
+  const [selectedCategories , setSelectedCategories]=useState([]);
+  const handleSelect=(val:string)=>{
+    setSelectedCategories((prev:string[])=>prev.find((p)=>p===val)
+    ?prev.filter((cat)=>cat!==val):[...prev,val]);
+
+  }
+
+
+
+  const handlePress =async ()=>{
+    const URL =`https://newsdata.io/api/1/news?apikey=${API_KEY}&country=in&language=en${selectedCategories.length>0 ? `&category=${selectedCategories.join()}` :""
+  
+  }`;
+  
+  try{
+    await fetch(URL)
+    .then((res)=>res.json())
+    .then((data)=>setNewsData(data.results));    
+  } catch (err) {
+    console.log(err);
+  }  
+
+  };
+  console.log(Object.keys(newsData[0]))
+
+
+  return (
+    <View style={styles.container}>
+      <Appbar.Header>
+         <Appbar.Content title="Home"></Appbar.Content>
+      </Appbar.Header>
+      <View style={styles.filtersContainer}>
+        {categories.map((cat)=>(
+          <Chip
+             key={cat}
+             mode="outlined"
+             style={styles.chipItem}
+             textStyle={{fontWeight:"400",color:"white",padding:1}}
+             showSelectedOverlay
+             selected={selectedCategories.find((c)=>cat===c)? true :false}
+             onPress={()=>handleSelect(cat)}
+             
+             >
+              {cat}
+             </Chip>
+        ))}
+        <Button
+        mode='outlined'
+         style={styles.button}
+        labelStyle={{fontSize:14,margin:"auto",color:theme.colors.primary}} 
+        icon={"sync"} 
+        onPress={handlePress}>
+          Refresh
+        </Button>
+       
+        
+
+
+      </View>
+
+      
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+
+  filtersContainer:{
+    flexDirection:"row",
+    flexWrap:"wrap",
+    marginVertical:10,
+
+  },
+  chipItem:{
+    marginHorizontal:5,
+    marginVertical:5,
+  },
+
+  button:{
+    maxWidth:400,
+    padding:0,
+    maxHeight:40,
+    
+  }
+  
+})
